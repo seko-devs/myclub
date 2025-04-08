6cff17cc60a106c8b520984e3c1eef34af9dd132
seko <git config --global user.email you@example.comyou@example.com>
2025-04-08 16:37:30 +0200

    app.js bestand met config van firestore

---
+++ app.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDgn1WcaTx8Aj8xwFrnxr4-ndYhoiJD1Bo",
+    authDomain: "myclub-fb-1ec24.firebaseapp.com",
+    projectId: "myclub-fb-1ec24",
+    storageBucket: "myclub-fb-1ec24.firebasestorage.app",
+    messagingSenderId: "796401924413",
+    appId: "1:796401924413:web:7b8a085d0d9709ee006d54"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
