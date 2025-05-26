c41ab744b77d4533c67ec9a0cef326f423da718d
seko <seyfietem@gmail.com>
2025-05-26 18:29:45 +0200

    fix firestore

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 
   // Init Auth and Firestore
     const auth = firebase.auth();
-    const db = firebase.firestore();
+    //const db = firebase.firestore();
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getFirestore, doc, getDoc } from 'firebase/firestore/lite';
+import { getFirestore } from 'firebase/firestore';
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -22,6 +23,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 async function getSinglePlayer() {
   const docRef = doc(db, "players", "EteZoTtd40FPI0aYjEKw"); // Assuming a document with ID "SF" exists
