249fe3230796e42a1e9c4397e4bac995b4d8b7e8
seko <seyfietem@gmail.com>
2025-06-01 14:09:08 +0200

    fix firebase modular

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
   // Init Auth and Firestore
-    const auth = auth();
+    //const auth = firebase.auth();
     //const db = firebase.firestore();
 
 // Import the functions you need from the SDKs you need
@@ -9,6 +9,8 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
 
 
 // Your web app's Firebase configuration
@@ -24,6 +26,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app); 
 
 async function getSinglePlayer() {
   const docRef = doc(db, "players", "EteZoTtd40FPI0aYjEKw"); // Assuming a document with ID "SF" exists
@@ -39,5 +42,13 @@ async function getSinglePlayer() {
     console.error("Error getting document:", e);
   }
 }
+
+//eventlisteners
+document.addEventListener('DOMContentLoaded', () => {
+  const getSinglePlayerButton = document.getElementById('getSinglePlayerButton');
+  if (getSinglePlayerButton) {
+      getSinglePlayerButton.addEventListener('click', getSinglePlayer);
+  }
+});
 
 
