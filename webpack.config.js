db6bcd5fa0aac2447c45e7c37cc5cbf59282844c
seko <seyfietem@gmail.com>
2026-03-29 00:34:38 +0100

    production source map change

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  // The entry point file described above
+  
+  mode: 'production',
+ 
+  // The entry point file described above 
   entry: './app.js',
   // The location of the build folder described above
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    clean: true, // Clean the output directory before emit
   },
   module: {
     rules: [
@@ -35,5 +39,5 @@ module.exports = {
   },
   // Optional and for development only. This provides the ability to
   // map the built code back to the original source format when debugging.
-  devtool: 'eval-source-map',
+  devtool: 'source-map',
 };
